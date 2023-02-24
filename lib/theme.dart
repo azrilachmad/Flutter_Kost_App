a618fa539dc2c67cedc2058fc6873de3d8c74295
Azril Achmad <azril.achmad@smt-id.com>
2023-02-24 19:13:44 +0700

    Adjust color theme

---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xFF3BACA5);
+Color secondaryColor = const Color(0xffF66D6D);
+Color blackColor = const Color(0xff000000);
+Color greyColor = const Color(0xff7A7E86);
