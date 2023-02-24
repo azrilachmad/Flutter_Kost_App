979c82fe9cef2830ba28e65e7e2ddfe6bde88842
Azril Achmad <azril.achmad@smt-id.com>
2023-02-24 19:19:18 +0700

    Adjust Font theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color primaryColor = const Color(0xFF3BACA5);
 Color secondaryColor = const Color(0xffF66D6D);
 Color blackColor = const Color(0xff000000);
+Color whiteColor = const Color(0xffFFFFFF);
 Color greyColor = const Color(0xff7A7E86);
+
+TextStyle primaryText = GoogleFonts.poppins(
+  color: primaryColor,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle textBlack = GoogleFonts.poppins(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle textWhite = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle textGrey = GoogleFonts.poppins(
+  color: greyColor,
+  fontWeight: FontWeight.w500,
+);
